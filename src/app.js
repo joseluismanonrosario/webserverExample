fef65245db32fdc1657ff96f3f0dfae4ffa83f24
Jose Luis Mañón <joseluismanonrosario@gmail.com>
2026-02-24 09:08:55 -0400

    Agrega Index y About page al servidor

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const port = process.env.port || 3000;
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello 6to. Bienvenidos a la clase de Node.js");
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
 app.listen(port, () => {
